24f06dea80ace4d35254f9e4b4df2f3043525256
Mark Vilrokx <mvilrokx@gmail.com>
2011-07-21 15:26:56 -0700

    changed extract for V2 plan extract

--- app/controllers/agilefant_controller.rb
+++ app/controllers/agilefant_controller.rb
@@ -1,19 +1,25 @@
 class AgilefantController < ApplicationController
+  RELEASE = "V2.0"
+  PRODUCT = "HXT"
+  DEFAULT_DELIVERABLE_TYPE = "6: Construct.6-1: Code"
+  ORGANIZATION = "Development"
+
   def plan
+
     @projects = AfBacklog.all(:include => {:stories => {:children => :tasks}},
                               :conditions => ['backlogs.backlogtype = "Project" and stories.parent_id is null'])
 
     csv_string = FasterCSV.generate do |csv|
 
-      cols = %w(Outline_Level WBS Name Scheduled_Work Percent_Complete Start_Date Finish_Date Resource_Names Activity Deliverable_Type Product Hyperlink Notes Release_Version)
+      cols = %w(Outline_Level WBS Name Scheduled_Work Percent_Complete Start_Date Finish_Date Resource_Names Activity Deliverable_Type Product Hyperlink Notes Release_Version Organization)
       csv << cols
 
       @projects.each do |project|
-        csv << [1, "P-#{project.id}", project.name, nil, nil, nil, nil, nil, nil, nil, "HXT", nil, nil, "V1.3"]
+        csv << [1, "P-#{project.id}", project.name, nil, nil, nil, nil, nil, nil, DEFAULT_DELIVERABLE_TYPE, PRODUCT, nil, nil, RELEASE, ORGANIZATION]
         project.stories.each do |story|
-          csv << [2, "T-#{story.id}", story.name, nil, nil, nil, nil, nil, nil, nil, "HXT", nil, nil, "V1.3"]
+          csv << [2, "T-#{story.id}", story.name, nil, nil, nil, nil, nil, nil, DEFAULT_DELIVERABLE_TYPE, PRODUCT, nil, nil, RELEASE, ORGANIZATION]
           story.children.each do |sub_story|
-            csv << ["3", "T-#{sub_story.id}", sub_story.name, nil, nil, nil, nil, nil, nil, nil, "HXT", nil, nil, "V1.3"]
+            csv << ["3", "T-#{sub_story.id}", sub_story.name, nil, nil, nil, nil, nil, nil, DEFAULT_DELIVERABLE_TYPE, PRODUCT, nil, nil, RELEASE, ORGANIZATION]
             sub_story.tasks.each do |task|
               csv << ["4",
                       "T-#{task.id}",
@@ -23,7 +29,7 @@ class AgilefantController < ApplicationController
                       task.story.backlog.startDate.strftime("%m/%d/%Y"),
                       task.story.backlog.endDate.strftime("%m/%d/%Y"),
                       task.developers.join(','),
-                      nil,nil,"HXT",nil,nil,"V1.3"]
+                      nil,DEFAULT_DELIVERABLE_TYPE,PRODUCT,nil,nil,RELEASE, ORGANIZATION]
             end
           end
         end
