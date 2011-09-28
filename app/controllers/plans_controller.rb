c8abc6f8ef0081e41695fa76675b1bc9cc5703f2
Mark Vilrokx <mvilrokx@gmail.com>
2011-09-28 15:45:48 -0700

    cleaned up PM UC report

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,9 +2,7 @@ class PlansController < ApplicationController
 	helper_method :sort_column, :sort_direction
 
   def product_managers_todo_list
-    puts "etner"
     @planned_paths = Plan.new.planned_paths
-    puts "leave"
   end
 
   def bpad
