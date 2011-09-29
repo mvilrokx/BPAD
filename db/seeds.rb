e5cfb2bb16594bc05fa3d15e8350a07dbc26215c
Mark Vilrokx <mvilrokx@gmail.com>
2011-09-29 09:49:11 -0700

    added some more validation to uploading Use Cases and developers

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,21 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-use_cases_file = "db/useCasesFile.csv"
+use_cases_file = "db/useCasesFile2.csv"
 developers = []
 open(use_cases_file) do |use_cases|
   puts "\nLoading Use Cases from file " + use_cases_file
   use_cases.read.each_line do |use_case|
     if !use_case.blank?
       business_process_name, name, description, developer_names = use_case.chomp.strip.split("|")
-      developers = developer_names.chomp.strip.split("#")
-      developers = User.all(:conditions => ["username IN (?)", developers])
+      assigned_developers = developer_names.chomp.strip.split("#")
+      assigned_developers.each do |username|
+        if user = User.first(:conditions => ["username = (?)", username])
+          developers << user
+        else
+          puts "Warning: #{username} was not found in BPAD as a User."
+        end
+      end
       bp = BusinessProcess.find_by_name(business_process_name)
       if bp
         path = Path.find_or_initialize_by_name(:name => name, :description => description, :business_process => bp)
