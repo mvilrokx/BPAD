0f05b9f1e99274f701b82b52c6ad79460f7bdab0
Mark Vilrokx <mvilrokx@gmail.com>
2011-10-05 11:54:27 -0700

    removed uploading of Use Cases from seeds.rb and put it as rake task

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,39 +9,3 @@
   Role.find_or_create_by_name role
 end
 
-use_cases_file = "db/useCasesFile.csv"
-developers = []
-open(use_cases_file) do |use_cases|
-  puts "\nLoading Use Cases from file " + use_cases_file
-  use_cases.read.each_line do |use_case|
-    if !use_case.blank?
-      business_process_name, name, description, tags = use_case.chomp.strip.split("|")
-#      business_process_name, name, description, developer_names, tags = use_case.chomp.strip.split("|")
-#      assigned_developers = developer_names.chomp.strip.split("#")
-      assigned_developers.each do |username|
-        if user = User.first(:conditions => ["username = (?)", username])
-          developers << user
-        else
-          puts "Warning: #{username} was not found in BPAD as a User."
-        end
-      end
-      bp = BusinessProcess.find_by_name(business_process_name)
-      if bp
-        path = Path.find_or_initialize_by_name(:name => name, :description => description, :business_process => bp)
-        path.users = []
-        path.users = developers
-        path.tags = []
-        path.tags = developers
-        path.do_not_track = true
-        path.save!
-        print "."
-      else
-        puts "Skipped loading of " + use_case + " because the Business Process it is attached to ('" + business_process_name + "') does not exist."
-
-      end
-    end
-    developers = []
-  end
-  puts "\nDone!"
-end
-
