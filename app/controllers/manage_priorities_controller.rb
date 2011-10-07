36c9229ba68e8145dc883868da51452d94edbb41
David Cohanoff <todav4-eweb@yahoo.com>
2011-10-06 22:33:48 -0700

    New Feature: added search by tag to Dashboard page

--- app/controllers/manage_priorities_controller.rb
+++ app/controllers/manage_priorities_controller.rb
@@ -1,9 +1,12 @@
 class ManagePrioritiesController < ApplicationController
-	before_filter :login_required
+  before_filter :login_required
 
 	def index
-    @all_paths = Path.find :all, :order => 'priority' #, :conditions => "priority IS NOT NULL"
+    @all_tags = get_all_tags
+#    @all_paths = Path.find :all, :order => 'priority' #, :conditions => "priority IS NOT NULL"
 #	  @other_paths = Path.find :all, :conditions => "priority IS NULL"
+#    all_tags
+     @all_paths = refine_search (params[:filters])
 	end
 
 	def prioritize_paths
@@ -14,5 +17,27 @@ class ManagePrioritiesController < ApplicationController
     end
     render :nothing => true
 	end
+
+
+	def get_all_tags
+     Tag.find :all, :order => 'name'
+	end
+
+
+  def refine_search(filters)
+#   filters = params[:filters]
+    if filters
+      Path.find :all, :order => 'priority' ,
+                      :conditions =>  [" id  in (select path_id from taggings t  where t.tag_id  in ( ? ) )" , filters]
+    else
+      Path.find :all, :order => 'priority' #, :conditions => "priority IS NOT NULL"
+    end
+
+  end
+
+
+
+
+
 end
 
