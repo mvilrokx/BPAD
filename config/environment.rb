186026714604869cbefe8c71f1edc5c541c0f360
Mark Vilrokx <mvilrokx@gmail.com>
2011-12-12 11:45:46 -0800

    removed gems from env file with move to bundler

--- config/environment.rb
+++ config/environment.rb
@@ -19,20 +19,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "acts_as_tree"
-  config.gem "paperclip"
-  config.gem "nokogiri"
-#  config.gem "omniauth"
-	config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
-	config.gem 'paper_trail', :version => '~> 1'
-	config.gem 'aasm', :version => '~> 2'
-  config.gem "compass", :version => '>= 0.11.1'
-  config.gem "haml"
-  config.gem "fastercsv", :version => "~> 1.5.4"
-	config.gem "ruport"
-
-	# Only needed in DEV
-	config.gem 'awesome_print', :as => 'ap'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
